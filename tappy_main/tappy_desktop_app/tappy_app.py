f047d9a531865830b314dd34fb4d0e3be1154993
Uzair Qidwai <uzairqidwai1@gmail.com>
2022-06-23 19:26:07 -0400

    Updates

---
+++ tappy_main/tappy_desktop_app/tappy_app.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import serial
+import time
+import serial.tools.list_ports
+
+
+def program():
+        value = str(id.get())
+        value = value + "#"
+        ser = serial.Serial(arduino_port, 9600)
+        time.sleep(1)
+        ser.write(bytes("W", 'UTF-8'))
+        ser.write(bytes(value, 'UTF-8'))
+        print("Written")
+        success()
+
+
+
+def read():
+    ser = serial.Serial(arduino_port, 9600)
+    time.sleep(1)
+    ser.write(bytes("R", 'UTF-8'))
+    cc=str(ser.readline())
+    id = cc[2:][:-5]
+    print(id)
+
+
+def success():
+    MsgBox = messagebox.askquestion ('Success!','Success! Would you like to program another?',icon = 'info')
+    if MsgBox == 'no':
+       root.destroy()
+    if MsgBox == 'yes':
+        id_entry.delete(0, END)
+
+def find_port():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        print (p)
+    if "Arduino Leonardo" in p.description:
+        return p.device
+
+
+
+
+
+
+arduino_port = find_port()
+
+root = Tk()
+root.title("ID Card Application")
+root.iconbitmap("C:/Users/Uzair Qidwai/Documents/GitHub/tappy/tappy_programming_app/tappy_icon.ico")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+id = StringVar()
+id_entry = ttk.Entry(mainframe, width=7, textvariable=id)
+id_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Program", command=program).grid(column=3, row=1, sticky=W)
+ttk.Button(mainframe, text="Read", command=read).grid(column=1, row=2, columnspan=3, sticky=W+E)
+
+ttk.Label(mainframe, text="ID Number").grid(column=1, row=1, sticky=W)
+ttk.Label(mainframe, text="Hold The Card On The Reader").grid(column=1, row=0, sticky=E)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+id_entry.focus()
+root.bind("<Return>", program)
+
+
+root.mainloop()
--- tappy_programming_app/tappy_app_updated/tappy_app_updated.ino
+++ tappy_programming_app/tappy_app_updated/tappy_app_updated.ino
@@ -34,12 +34,112 @@ void setup() {
   Serial.begin(9600);        // Initialize serial communications with the PC
   SPI.begin();               // Init SPI bus
   mfrc522.PCD_Init();        // Init MFRC522 card
-  Serial.println(F("Write personal data on a MIFARE PICC "));
+  //Serial.println(F("Write personal data on a MIFARE PICC "));
+}
+void loop() {
+  Menu();
 }
 
-void loop() {
 
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+
+void Menu() {
+    for (;;) {
+        switch (Serial.read()) {
+            case 'R': read_card(); break;
+            case 'W': write_card(); break;
+            default: continue;  // includes the case 'no input'
+        }
+    }
+}
+
+void read_card(){
+
+
+// Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  //-------------------------------------------
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  //Serial.println(F("**Card Detected:**"));
+
+  //-------------------------------------------
+
+  //mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
+
+  //-------------------------------------------
+
+  //Serial.print(F("ID Number: "));
+
+  byte buffer1[18];
+
+  block = 4;
+  len = 18;
+
+
+  //---------------------------------------- GET ID NUMBER
+
+  byte buffer2[18];
+  block = 1;
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
+  if (status != MFRC522::STATUS_OK) {
+    //Serial.print(F("Authentication failed: "));
+    //Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  if (status != MFRC522::STATUS_OK) {
+    //Serial.print(F("Reading failed: "));
+    //Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  //PRINT ID NUMBER TO SERIAL & HID
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.write(buffer2[i]);
+  }
+  Serial.write("\n");
+
+  //----------------------------------------
+
+  //Serial.println(F("\n**End Reading**\n"));
+
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+
+}
+
+
+
+
+
+
+
+
+void write_card(){
+
+    // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 
@@ -112,16 +212,7 @@ void loop() {
     Serial.println(F("MIFARE_Write() success: "));
   }
 
-  char mode;
-  char current_mode;
-  char mode_arr[2];
-  mode = Serial.readBytesUntil('$', (char *) mode_arr, 2);
-  
-  Serial.print("I received: ");
-  Serial.println(mode_arr[1]);
-
   Serial.println(" ");
   mfrc522.PICC_HaltA(); // Halt PICC
   mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
-
 }
